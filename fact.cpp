278df29acef44759efb4c55f672afc7e501476d0
Vaughan Kitchen <contact@tophat.nz>
2024-08-19 17:11:34 +1200

    Add iterative implementation of factorial

--- fact.cpp
+++ fact.cpp
@@ -1,5 +1,16 @@
 int fact(int n) {
+	if (n == 0)
+		return 1;
+
+	int res = n;
+	for (; n > 1; n--)
+		res *= n - 1;
+
+	return res;
+}
+
+int fact_r(int n) {
 	if (n <= 1)
 		return 1;
-	return n * fact(n - 1);
+	return n * fact_r(n - 1);
 }
