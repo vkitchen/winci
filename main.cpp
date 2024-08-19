d10c5ffd3175d27d9c5e35554e5ddbcf6fa7513f
Vaughan Kitchen <contact@tophat.nz>
2024-08-19 12:58:23 +1200

    Add fibonacci as library code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "fib.h"
 
 int main(int argc, char** argv) {
 	std::cout << "Hello, World!" << std::endl;
+	
+	for (int i = 0; i < 11; i++)
+		std::cout << i << ": " << fib(i) << std::endl;
 
 	return 0;
 }
